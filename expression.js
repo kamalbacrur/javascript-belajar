02c0bb8808821a0db7372f1d88a7b0637bd67d8b
Muhamad Kamal <kmuhamad361@gmail.com>
2019-04-03 08:49:08 +0700

    add reguler Expression & flags

---
+++ expression.js
@@ -0,0 +1,15 @@
+function regularExpress(value) {
+    let str = "abcdefghijklmnopqrstuvwxyz"
+
+    return str.search(/b/)
+}
+console.log(regularExpress())
+
+function panggilexpress(value) {
+    let str = "abcdefghijklmnopqrstuvwxyz"
+
+    console.log(str.search(/C/))
+    console.log(str.search(/R/i))
+    console.log(str.search(/F/i))
+}
+panggilexpress() 
